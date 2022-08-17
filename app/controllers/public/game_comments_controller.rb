61ca00f57de397fcd9eab49dc13d23a4232d20f1
EC2 Default User <k9batai33@gmail.com>
2022-08-17 13:06:12 +0000

    コメント機能　修正

--- app/controllers/public/game_comments_controller.rb
+++ app/controllers/public/game_comments_controller.rb
@@ -5,13 +5,18 @@ class Public::GameCommentsController < ApplicationController
     @game_comment = @game.game_comments.new(game_comment_params)
     @game_comment.end_user_id = current_end_user.id
     if @game_comment.save
-      redirect_to request.referer
+      redirect_to game_path(@game)
+    else
+      @game = Game.find_by(params[:id])
+      @game_comments = @game.game_comments
+      render 'public/games/show'
     end
   end
 
   def destroy
+    game = Game.find_by(params[:id])
     GameComment.find_by(id: params[:id], game_id: params[:game_id]).destroy
-    redirect_to request.referer
+    redirect_to game_path(game)
   end
 
   private
