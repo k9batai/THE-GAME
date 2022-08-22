2bd0ebc7778ba1db6cf212c0ee3517816c3e8f35
EC2 Default User <k9batai33@gmail.com>
2022-08-22 03:12:07 +0000

    コメント機能　修正

--- app/controllers/public/game_comments_controller.rb
+++ app/controllers/public/game_comments_controller.rb
@@ -1,15 +1,14 @@
 class Public::GameCommentsController < ApplicationController
 
   def create
-    @game = Game.find(params[:game_id])
-    @game_comment = @game.game_comments.new(game_comment_params)
-    @game_comment.end_user_id = current_end_user.id
-    @game_comment.save
-    redirect_to game_path(@game), notice: 'コメントしました。'
+    game = Game.find(params[:game_id])
+    game_comment = game.game_comments.new(game_comment_params)
+    game_comment.end_user_id = current_end_user.id
+    game_comment.save
+    redirect_to game_path(game), notice: 'コメントしました。'
   end
 
   def destroy
-    @game = Game.find_by(params[:id])
     GameComment.find_by(id: params[:id], game_id: params[:game_id]).destroy
     redirect_to game_path(params[:game_id]), notice: 'コメントを削除しました。'
   end
--- app/models/game_comment.rb
+++ app/models/game_comment.rb
@@ -1,8 +1,6 @@
 class GameComment < ApplicationRecord
-
   belongs_to :end_user
   belongs_to :game
 
-  validates :comment, presence: true
-
+  validates :comment, presence: true, length: { in: 1..50 }
 end
