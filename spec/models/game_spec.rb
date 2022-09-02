80d84ff75a73f425c6a8bc526babe4fe318b60d9
EC2 Default User <k9batai33@gmail.com>
2022-09-02 13:38:10 +0000

    Gameモデルのテストコード追加

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe 'Gameモデルのテスト', type: :model do
         game.title = ''
         is_expected.to eq false
       end
-      it '20文字以下であること: 20文字はOK' do
-        game.title = Faker::Lorem.characters(number: 20)
-        is_expected.to eq true
-      end
       it '20文字以下であること: 21文字はNG' do
         game.title = Faker::Lorem.characters(number: 21)
         is_expected.to eq false
@@ -22,8 +18,27 @@ RSpec.describe 'Gameモデルのテスト', type: :model do
     end
 
     context 'bodyカラム' do
-      it '空腹でないこと' do
+      it '空欄でないこと' do
         game.introduction = ''
+        is_expected.to eq false
+      end
+      it '300文字以下であること: 301文字はNG' do
+        game.introduction = Faker::Lorem.characters(number: 301)
+        is_expected.to eq false
+      end
+      it '15文字以上であること: 14文字はNG' do
+        game.introduction = Faker::Lorem.characters(number: 14)
+        is_expected.to eq false
+      end
+    end
+
+    context 'categoryカラム' do
+      it '空欄でないこと' do
+        game.category = ''
+        is_expected.to eq false
+      end
+      it '10文字以下であること: 11文字はNG' do
+        game.category = Faker::Lorem.characters(number: 11)
         is_expected.to eq false
       end
     end
